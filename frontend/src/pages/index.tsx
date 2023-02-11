9f90886501f7b2151ba0bc16d0124340bddc7705
jzenzinger <juris.zen@seznam.cz>
2023-02-11 17:50:12 +0100

    Working signin

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,23 +6,26 @@ const Home: NextPage = () => {
 
     console.log("HERE IS DATA", data);
 
+    function handleSignIn(type: string) {
+        event.preventDefault();
+        signIn();
+    }  
+
     return (
         <section className="h-screen">
             <div className="px-6 h-full text-gray-800">
-                <form className='mt-10 mx-auto'>
                     {
                         data?.user ? (
-                            <button onClick={() => signOut()} 
+                            <button onClick={() => signOut()}
                                     className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                                         Sign Out
                             </button>
                         ) : (
-                            <button onClick={() => signIn('google')} 
+                            <button onClick={() => handleSignIn('google')}
                                     className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                                         Sign In
                             </button>
                     )}
-                </form>
                 <br></br>
                 <br></br>
                 {data?.user?.name}
