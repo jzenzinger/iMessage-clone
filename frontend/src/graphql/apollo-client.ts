ba3db814818102f14ab132bfc0080b50a5ff1f30
jzenzinger <juris.zen@seznam.cz>
2023-02-13 09:04:51 +0100

    Apollo init with httpLink on FE

--- frontend/src/graphql/apollo-client.ts
+++ frontend/src/graphql/apollo-client.ts
@@ -1,5 +1,15 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
+// Later will be here WebSocket link for realtime chat data
+
 const httpLink = new HttpLink({
-    uri: 'http://localhost:4000/graphql'
+    // Key difference between REST API, Graphql API
+    // Everything is passing by one API (graphql), Rest API have multiple things
+    uri: 'http://localhost:4000/graphql',
+    credentials: "include"
+});
+
+export const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
 })
