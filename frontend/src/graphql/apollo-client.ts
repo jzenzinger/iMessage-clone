a95263583c1525ece65baf574237c117b13cb6d3
jzenzinger <juris.zen@seznam.cz>
2023-03-01 08:40:59 +0100

    WebSocket init for support

--- frontend/src/graphql/apollo-client.ts
+++ frontend/src/graphql/apollo-client.ts
@@ -1,15 +1,40 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { createClient } from "graphql-ws";
 
 // Later will be here WebSocket link for realtime chat data
 
 const httpLink = new HttpLink({
-    // Key difference between REST API, Graphql API
-    // Everything is passing by one API (graphql), Rest API have multiple things
-    uri: 'http://localhost:4000/graphql',
-    credentials: "include"
+  // Key difference between REST API, Graphql API
+  // Everything is passing by one API (graphql), Rest API have multiple things
+  uri: "http://localhost:4000/graphql",
+  credentials: "include",
 });
 
+// FE is now supporting WebSockets
+const wsLink =
+  typeof window !== "undefined"
+    ? new GraphQLWsLink(
+        createClient({
+          url: "ws://localhost:4000/graphql/subscriptions",
+        })
+      )
+    : null;
+
+const link = typeof window !== "undefined" && wsLink != null ? split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    httpLink,
+  ) : httpLink;
+
 export const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
-})
+  link,
+  cache: new InMemoryCache(),
+});
