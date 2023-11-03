af1b66bfeb1d2fab0ec928bb23e3779cd5cdee6e
jzenzinger <juris.zen@seznam.cz>
2023-11-03 11:29:34 +0100

    Reformat and error update

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import { client } from '../graphql/apollo-client';
-import '../styles/globals.css';
-import { ApolloProvider } from '@apollo/client';
-import { SessionProvider } from 'next-auth/react';
-import type { AppProps } from 'next/app'
-import { Toaster } from 'react-hot-toast';
+import { client } from "@/graphql/apollo-client";
+import "../styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
-export default function App({ Component, pageProps: { session, ...pageProps} }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
